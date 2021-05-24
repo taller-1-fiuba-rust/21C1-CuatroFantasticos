20f522b5365992ffafeab353d17881869a2208d8
Manzano Lautaro <lautaro.manzano@mercadolibre.com>
2021-05-24 20:15:44 -0300

    adds architecture

---
+++ src/architecture.rs
@@ -0,0 +1,3 @@
+pub mod message;
+pub mod thread_pool;
+pub mod worker;
---
+++ src/architecture/worker.rs
@@ -0,0 +1,41 @@
+use crate::architecture::message::Message;
+use std::sync::{mpsc, Arc, Mutex};
+use std::thread;
+use std::thread::JoinHandle;
+
+pub struct Worker {
+    id: usize,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+impl Worker {
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let message = receiver.lock().unwrap().recv().unwrap();
+
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+
+                    job();
+                }
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate.", id);
+
+                    break;
+                }
+            }
+        });
+
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+    pub fn id(&self) -> usize {
+        self.id
+    }
+    pub fn thread(&mut self) -> &mut Option<JoinHandle<()>> {
+        &mut self.thread
+    }
+}
