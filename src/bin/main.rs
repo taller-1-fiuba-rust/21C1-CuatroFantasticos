d9f3cd69a1dec7955a778c1628d176128ab9ab50
Manzano Lautaro <lautaro.manzano@mercadolibre.com>
2021-05-30 22:50:46 -0300

    implements parsing of configuration

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,11 @@
 use redis_server::architecture::server;
-use std::{env, fs};
+use redis_server::configuration::Configuration;
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let filename = &args[1];
-
+    let _conf = Configuration::new(filename);
 
     server::run_server();
-
 }
--- src/configuration.rs
+++ src/configuration.rs
@@ -1,18 +1,35 @@
+use std::collections::HashMap;
 use std::fs;
 
 pub struct Configuration {
-    verbose: usize,
-    port: usize,
-    timeout: usize,
-    dbfilename: String,
-    logfile: String,
+    conf: HashMap<String, ConfValue>,
+}
+
+pub enum ConfValue {
+    Usize(usize),
+    String(String),
 }
 
 impl Configuration {
-    pub fn new(filename: String) -> Configuration {
-        let contents = fs::read_to_string(filename)
-            .expect("Something went wrong reading the file");
-        // TODO: pasar del contents a los atributos del conf
-        Configuration {verbose, port, timeout, dbfilename, logfile}
+    pub fn new(filename: &str) -> Configuration {
+        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+        let mut conf: HashMap<String, ConfValue> = HashMap::new();
+        for line in contents.lines() {
+            let split = line.split(':');
+            let parsed_line: Vec<&str> = split.collect();
+            let usize_fields: Vec<&str> = Vec::from(["verbose", "port", "timeout"]);
+            let value;
+            if usize_fields.contains(&parsed_line[0]) {
+                value = ConfValue::Usize(parsed_line[1].trim().parse().unwrap());
+            } else {
+                value = ConfValue::String(parsed_line[1].to_owned());
+            };
+            conf.insert(parsed_line[0].to_owned(), value);
+        }
+        Configuration { conf }
+    }
+
+    pub fn get(&self, key: &str) -> &ConfValue {
+        self.conf.get(key).unwrap()
     }
 }
