3afdb4e31d98bac16d49df1ea087f5483d159083
Serra123 <juanx.serra.jcs@gmail.com>
2021-06-02 10:37:42 -0300

    changes implementation of ConfigValue

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,27 +5,40 @@ pub struct Configuration {
     conf: HashMap<String, ConfValue>,
 }
 
+#[derive(Debug)]
 pub enum ConfValue {
-    Usize(usize),
-    String(String),
+    Verbose(usize),
+    Timeout(usize),
+    Port(usize),
+    Dbfilename(String),
+    LogFile(String),
+}
+
+impl ConfValue {
+    pub fn new(key: &str, value: &str) -> ConfValue {
+        match key {
+            "verbose" => ConfValue::Verbose(value.trim().parse().unwrap()),
+            "timeout" => ConfValue::Timeout(value.trim().parse().unwrap()),
+            "port" => ConfValue::Port(value.trim().parse().unwrap()),
+            "dbfilename" => ConfValue::Dbfilename(value.trim().to_owned()),
+            "logfile" => ConfValue::LogFile(value.trim().to_owned()),
+            _ => panic!("{:?}", "ERROR AL PARSEAR CONFIG"),
+        }
+    }
 }
 
 impl Configuration {
     pub fn new(filename: &str) -> Configuration {
-        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
         let mut conf: HashMap<String, ConfValue> = HashMap::new();
+
+        let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
         for line in contents.lines() {
             let split = line.split(':');
             let parsed_line: Vec<&str> = split.collect();
-            let usize_fields: Vec<&str> = Vec::from(["verbose", "port", "timeout"]);
-            let value;
-            if usize_fields.contains(&parsed_line[0]) {
-                value = ConfValue::Usize(parsed_line[1].trim().parse().unwrap());
-            } else {
-                value = ConfValue::String(parsed_line[1].to_owned());
-            };
+            let value = ConfValue::new(parsed_line[0], parsed_line[1]);
             conf.insert(parsed_line[0].to_owned(), value);
         }
+        println!("{:?}", conf);
         Configuration { conf }
     }
 
