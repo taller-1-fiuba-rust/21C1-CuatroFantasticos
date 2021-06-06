935b7f2503d2ca56a67a87c5abfc83be10901d46
Manzano Lautaro <lautaro.manzano@mercadolibre.com>
2021-06-06 12:40:08 -0300

    refactors to string --> config

--- redis_server/src/architecture/server.rs
+++ redis_server/src/architecture/server.rs
@@ -7,8 +7,7 @@ use std::time::Duration;
 const THREAD_MAX_QUANTITY: usize = 1000;
 
 pub fn run_server(conf: Configuration) {
-    let port = conf.get("port");
-    let addr = "127.0.0.1:".to_owned() + port;
+    let addr = "127.0.0.1:".to_owned() + conf.get("port");
     let listener = TcpListener::bind(addr).unwrap();
     let pool = ThreadPool::new(THREAD_MAX_QUANTITY);
 
--- redis_server/src/configuration.rs
+++ redis_server/src/configuration.rs
@@ -2,63 +2,23 @@ use std::collections::HashMap;
 use std::fs;
 
 pub struct Configuration {
-    conf: HashMap<String, ConfValue>,
-}
-
-#[derive(Debug)]
-pub enum ConfValue {
-    Verbose(String),
-    Timeout(String),
-    Port(String),
-    Dbfilename(String),
-    LogFile(String),
-}
-
-impl ConfValue {
-    pub fn new(key: &str, value: &str) -> ConfValue {
-        match key {
-            "verbose" => ConfValue::Verbose(value.trim().parse().unwrap()),
-            "timeout" => ConfValue::Timeout(value.trim().parse().unwrap()),
-            "port" => ConfValue::Port(value.trim().to_owned()),
-            "dbfilename" => ConfValue::Dbfilename(value.trim().to_owned()),
-            "logfile" => ConfValue::LogFile(value.trim().to_owned()),
-            _ => panic!("{:?}", "ERROR AL PARSEAR CONFIG"),
-        }
-    }
-
-    pub fn get(&self) -> &String {
-        match self {
-            ConfValue::Port(value) => value,
-            ConfValue::Verbose(value) => value,
-            ConfValue::Timeout(value) => value,
-            ConfValue::Dbfilename(value) => value,
-            ConfValue::LogFile(value) => value,
-        }
-    }
+    conf: HashMap<String, String>,
 }
 
 impl Configuration {
     pub fn new(filename: &str) -> Configuration {
-        let mut conf: HashMap<String, ConfValue> = HashMap::new();
-
-        // Agregamos valores predeterminados
-        conf.insert(
-            "dbfilename".to_owned(),
-            ConfValue::new("dbfilename", "dump.rdb"),
-        );
-
+        let mut conf: HashMap<String, String> = HashMap::new();
         let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
         for line in contents.lines() {
             let split = line.split(':');
             let parsed_line: Vec<&str> = split.collect();
-            let value = ConfValue::new(parsed_line[0], parsed_line[1]);
-            conf.insert(parsed_line[0].to_owned(), value);
+            conf.insert(parsed_line[0].to_owned(), parsed_line[1].trim().to_owned());
         }
         println!("{:?}", conf);
         Configuration { conf }
     }
 
     pub fn get(&self, key: &str) -> &String {
-        self.conf.get(key).unwrap().get()
+        self.conf.get(key).unwrap()
     }
 }
