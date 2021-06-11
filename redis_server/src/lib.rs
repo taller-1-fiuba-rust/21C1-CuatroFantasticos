b734eebbba520a15109ae348bac98864f1276f1e
Facundo Bravo <facundo.bravo@mercadolibre.com>
2021-06-10 21:49:35 -0300

    Adds request_handler initial structure

--- redis_server/src/lib.rs
+++ redis_server/src/lib.rs
@@ -1,1 +1,2 @@
 pub mod architecture;
+mod request_handler;
---
+++ redis_server/src/request_handler/parser.rs
@@ -0,0 +1,13 @@
+struct Parser {
+
+}
+
+impl Parser {
+    pub fn new() -> Self {
+        Parser {}
+    }
+    pub fn parse(&self, message: &[u8]) -> Self {
+        Parser {}
+    }
+}
+
