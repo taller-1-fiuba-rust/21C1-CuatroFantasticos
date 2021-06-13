1505cf27b1a63d171f481e337bc89b01228ccaa0
FilyanKaragoz <filyan.karagoz@mercadolibre.com>
2021-06-12 21:48:53 -0300

    adds tests for threadpool

--- redis_server/src/architecture/message.rs
+++ redis_server/src/architecture/message.rs
@@ -1,5 +1,6 @@
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
+
 pub enum Message {
     NewJob(Job),
     Terminate,
--- redis_server/src/architecture/worker.rs
+++ redis_server/src/architecture/worker.rs
@@ -3,6 +3,7 @@ use std::sync::{mpsc, Arc, Mutex};
 use std::thread;
 use std::thread::JoinHandle;
 
+#[derive(Debug)]
 pub struct Worker {
     id: usize,
     thread: Option<thread::JoinHandle<()>>,
@@ -38,4 +39,16 @@ impl Worker {
     pub fn thread(&mut self) -> &mut Option<JoinHandle<()>> {
         &mut self.thread
     }
+}
+#[cfg(test)]
+mod test {
+    use crate::architecture::worker::Worker;
+
+
+#[test]
+    fn create_new_worker_has_no_errors() {
+
+    //let a = Worker::new(2,);
+}
+
 }
