1533b80abb2e56efb3296a044753a484b4ee39b7
Manzano Lautaro <lautaro.manzano@mercadolibre.com>
2021-06-26 22:50:39 -0300

    por si las dudas

--- redis_server/tests/server_test.rs
+++ redis_server/tests/server_test.rs
@@ -1,1 +1,22 @@
+/*use redis::ConnectionLike;
+use redis_server::architecture::server;
+use redis_server::configuration::Configuration;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
+#[test]
+fn test_timeout_should_disconnects_client() {
+    let conf = Configuration::new("../redis.conf");
+    thread::spawn(move || {
+        server::run_server(&conf);
+    });
+    sleep(Duration::new(1, 0));
+    let client = redis::Client::open("redis://127.0.0.1:7878/").unwrap();
+    let mut con = client
+        .get_connection()
+        .expect("falló la conexión cliente-servidor");
+    sleep(Duration::new(2, 0));
+    let is_connect = con.check_connection();
+    assert!(!is_connect);
+}*/
