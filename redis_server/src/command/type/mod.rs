a67171c9c5d0cc39a52a2c0b0b9e4031842751bf
Manzano Lautaro <lautaro.manzano@mercadolibre.com>
2021-07-05 11:24:13 -0300

    fix type command

--- redis_server/src/command/type/mod.rs
+++ redis_server/src/command/type/mod.rs
@@ -1,6 +1,7 @@
 use crate::command::RedisCommand;
 use crate::data::storage::accessor::StorageAccessor;
 use crate::data::storage::request_message::StorageRequestMessageEnum;
+use crate::data::storage::response_message::StorageResponseMessageEnum;
 use crate::protocol_serialization::ProtocolSerializer;
 
 pub struct RedisCommandType {
@@ -16,7 +17,17 @@ impl RedisCommandType {
 impl RedisCommand for RedisCommandType {
     fn execute(&self, accessor: StorageAccessor) -> Result<String, String> {
         let response = accessor.access(StorageRequestMessageEnum::Type(self.key.clone()))?;
-        let response = response.get_value().protocol_serialize_to_simple_string();
+        let response = match response.get_value() {
+            StorageResponseMessageEnum::RedisValue(value) => {
+                StorageResponseMessageEnum::String(value.get_type())
+                    .protocol_serialize_to_simple_string()
+            }
+            StorageResponseMessageEnum::Error(value) => {
+                StorageResponseMessageEnum::Error(value.to_string())
+                    .protocol_serialize_to_simple_string()
+            }
+            _ => "Client did not receive a correct response from the database".to_string(),
+        };
         Ok(response)
     }
 }
