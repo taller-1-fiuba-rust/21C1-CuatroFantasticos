c9cdd77474531f0f95f66f0465c255aaaf6b21d4
Facundo Bravo <facundo.bravo@mercadolibre.com>
2021-07-09 22:38:08 -0300

    Removes commented code

--- redis_server/src/data/redis_value/mod.rs
+++ redis_server/src/data/redis_value/mod.rs
@@ -56,24 +56,3 @@ impl ProtocolSerializer for RedisValue {
         }
     }
 }
-
-/*
-pub trait RedisValueClone {
-    fn clone_box(&self) -> Box<dyn RedisValue>;
-}
-
-impl<T> RedisValueClone for T
-where
-    T: 'static + RedisValue + Clone,
-{
-    fn clone_box(&self) -> Box<dyn RedisValue> {
-        Box::new(self.clone())
-    }
-}
-
-impl Clone for Box<dyn RedisValue> {
-    fn clone(&self) -> Box<dyn RedisValue> {
-        self.clone_box()
-    }
-}
-*/
