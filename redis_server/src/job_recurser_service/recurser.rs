cf1b5537dd49a0308b719ac5b722c2fc83893746
Facundo Bravo <facundo.bravo@mercadolibre.com>
2021-07-22 23:19:20 -0300

    Fixes bug in job recurser

--- redis_server/src/job_recurser_service/recurser.rs
+++ redis_server/src/job_recurser_service/recurser.rs
@@ -40,7 +40,7 @@ impl<T: RecurringJob + Send> JobRecurser<T> {
         let current_time = current_time_in_millis();
         if current_time + (WAIT_TIME as u128) < self.next_ts {
             sleep(Duration::from_millis(WAIT_TIME));
-        } else {
+        } else if current_time < self.next_ts {
             sleep(Duration::from_millis((self.next_ts - current_time) as u64));
         }
     }
@@ -57,5 +57,51 @@ impl<T: RecurringJob + Send> JobRecurser<T> {
             self.update_next_ts();
             self.wait();
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::job_recurser_service::recurser::{JobRecurser, JobRecurserMessage};
+    use crate::job_recurser_service::{RecurringJob, RecurringJobError};
+    use std::sync::mpsc;
+    use std::time::Instant;
+
+    pub struct Job1<'a> {
+        qty_executed: &'a mut usize,
+        sender: mpsc::Sender<JobRecurserMessage>,
+    }
+
+    impl<'a> Job1<'a> {
+        pub fn new(qty_executed: &'a mut usize, sender: mpsc::Sender<JobRecurserMessage>) -> Job1 {
+            Job1 {
+                qty_executed,
+                sender,
+            }
+        }
+    }
+
+    impl<'a> RecurringJob for Job1<'a> {
+        fn execute_job(&mut self) -> Result<(), RecurringJobError> {
+            *self.qty_executed += 1;
+            if *self.qty_executed >= 5 {
+                self.sender
+                    .send(JobRecurserMessage::Terminate)
+                    .map_err(|_| RecurringJobError {})?
+            }
+            Ok(())
+        }
+    }
+
+    #[test]
+    pub fn job_recurser_does_the_job() {
+        let (tx, rx) = mpsc::channel();
+        let mut qty = 0;
+        let job = Job1::new(&mut qty, tx);
+        let mut recurser = JobRecurser::new(job, rx, 2);
+        let start = Instant::now();
+        recurser.init();
+        assert_eq!(qty, 5);
+        assert!(start.elapsed().as_millis() < 100);
     }
 }
